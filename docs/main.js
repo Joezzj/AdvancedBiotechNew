8f5289bee2dbdd67437172d038c121813a2e3888
joe <joejianzhu@126.com>
2025-11-01 17:57:20 +1300

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -28,11 +28,29 @@ window.onload = function () {
   checkDevice(userAgent, elements); //menu-toggle show/hide
   //add click listner to menu-toggle
   elements.addEventListener("click", () => {
-    console.log(dropDown);
     dropDown.className == "nav-links"
       ? (dropDown.className = "nav-links active")
       : (dropDown.className = "nav-links");
   });
+
+  document
+    .getElementById("contactForm")
+    .addEventListener("submit", async function (e) {
+      document.getElementById("submit").disabled = true;
+      e.preventDefault();
+      const form = {
+        name: this.name.value,
+        email: this.email.value,
+        message: this.message.value,
+      };
+      const result = await sendEmail(form);
+      if (result.success) {
+        alert("successful sent");
+      } else {
+        alert("fail sent:" + result.error);
+      }
+      document.getElementById("submit").disabled = false;
+    });
 };
 
 /**
@@ -43,3 +61,23 @@ window.addEventListener("beforeunload", (event) => {
   const dropDown = document.getElementById("dropDown");
   dropDown.className = "nav-links";
 });
+
+/**
+ * api send request
+ */
+
+function sendEmail(form) {
+  return new Promise((resolve, reject) => {
+    fetch("http://localhost:5000/send_mail", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        resolve(data);
+      });
+  });
+}
